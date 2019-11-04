049737ccc601cab1fe89c8ec3278664bd0daf7d9
Chataya Lynderly <clynderl@mi-j4.21-school.ru>
2019-11-04 20:36:21 +0300

    solve fixed

--- srcs/solve.c
+++ srcs/solve.c
@@ -6,7 +6,7 @@
 /*   By: clynderl <clynderl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 13:27:03 by clynderl          #+#    #+#             */
-/*   Updated: 2019/11/04 19:55:13 by clynderl         ###   ########.fr       */
+/*   Updated: 2019/11/04 20:35:28 by clynderl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,10 @@ int		ft_is_placeable(t_tetri *tetri, t_map *map, int pos)
 	while ((tetri->data)[++i])
 	{
 		if (ft_isalpha((tetri->data)[i]))
-		{
 			if ((pos < 0 || pos >= map->area) ||
 			(pos >= 0 && pos < map->area && ft_isalpha((map->data)[pos]))
 			|| (pos >= 0 && pos < map->area && (map->data)[pos] == '\n'))
 				return (0);
-		}
 		if (pos >= 0 && pos < map->area &&
 		(map->data)[pos] == '\n' && ft_isalpha((tetri->data)[i]))
 			return (0);
@@ -47,17 +45,15 @@ int		ft_is_placeable(t_tetri *tetri, t_map *map, int pos)
 	return (1);
 }
 
-int		ft_backtracking(t_tetri *tetries, t_map *map)
+int		ft_backtracking(t_tetri *tetries, t_map *map, int pos)
 {
 	t_tetri *tetri;
 	int		i;
 	int		ls;
-	int		pos;
 
-	pos = 0;
 	ls = ft_list_size(tetries);
-	i = 0;
 	tetri = tetries;
+	i = 0;
 	while (i < ls)
 	{
 		if (tetri->pos == -1)
@@ -67,8 +63,6 @@ int		ft_backtracking(t_tetri *tetries, t_map *map)
 	}
 	if (i == ls)
 		return (1);
-	i = 0;
-	tetri = tetries;
 	while (pos < map->area)
 	{
 		while (tetri && (tetri->pos > 0))
@@ -77,7 +71,7 @@ int		ft_backtracking(t_tetri *tetries, t_map *map)
 		{
 			ft_fill(tetri, map, pos, 1);
 			tetri->pos = pos;
-			if (ft_backtracking(tetri, map))
+			if (ft_backtracking(tetri, map, 0))
 				return (1);
 			ft_fill(tetri, map, pos, 0);
 			tetri->pos = -1;
@@ -107,7 +101,7 @@ t_map	*ft_solve(t_tetri *tetries)
 			i++;
 		}
 		map->data[i] = '\0';
-		if (ft_backtracking(tetries, map))
+		if (ft_backtracking(tetries, map, 0))
 			return (map);
 		map->size++;
 	}
