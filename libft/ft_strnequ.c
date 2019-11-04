4dd78415b0a84552a0745bcbb89cc480792f6ac1
Chataya Lynderly <clynderl@mi-j4.21-school.ru>
2019-11-04 21:00:34 +0300

    unused var fix

--- libft/ft_strnequ.c
+++ libft/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: clynderl <clynderl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 15:03:00 by clynderl          #+#    #+#             */
-/*   Updated: 2019/09/16 19:18:17 by clynderl         ###   ########.fr       */
+/*   Updated: 2019/11/04 20:52:23 by clynderl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	size_t i;
-
-	i = 0;
 	if (!s1 || !s2)
 		return (0);
 	if (ft_strncmp(s1, s2, n))
